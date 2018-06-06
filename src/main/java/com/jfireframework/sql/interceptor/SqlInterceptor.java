2f4aaa8ac6165d06dd22102c658ba2ee01284f63
eric <eric@jfire.cn>
2018-06-06 15:14:26 +0800

    删除拦截器，后续该功能由执行器替代

--- src/main/java/com/jfireframework/sql/interceptor/SqlInterceptor.java
+++
@@ -1,9 +0,0 @@
-package com.jfireframework.sql.interceptor;
-
-import java.sql.Connection;
-import com.jfireframework.baseutil.order.Order;
-
-public interface SqlInterceptor extends Order
-{
-    void intercept(InterceptorChain chain, Connection connection, String sql, Object... params);
-}
