b1a43de5746b48bc01187739909b37689c889154
linbin <495561397@qq.com>
2023-09-17 10:10:48 +0800

    文本字符串使用"来进行包围。

--- src/main/java/com/jfirer/jsql/analyse/template/Template.java
+++ src/main/java/com/jfirer/jsql/analyse/template/Template.java
@@ -213,7 +213,7 @@ public class Template
                     {
                         if (mark != index)
                         {
-                            builder.append("print(`").append(content.substring(mark, index)).append("`);\r\n");
+                            builder.append("print(\"").append(content.substring(mark, index)).append("\");\r\n");
                         }
                         mark = index += 2;
                         type = IN_VARIABLE;
@@ -222,7 +222,7 @@ public class Template
                     {
                         if (mark != index)
                         {
-                            builder.append("print(`").append(content.substring(mark, index)).append("`);\r\n");
+                            builder.append("print(\"").append(content.substring(mark, index)).append("\");\r\n");
                         }
                         mark = index += 2;
                         type = IN_PARAM;
@@ -231,7 +231,7 @@ public class Template
                     {
                         if (mark != index)
                         {
-                            builder.append("print(`").append(content.substring(mark, index)).append("`);\r\n");
+                            builder.append("print(\"").append(content.substring(mark, index)).append("\");\r\n");
                         }
                         mark = index += 2;
                         type = IN_COLLECTION;
@@ -240,7 +240,7 @@ public class Template
                     {
                         if (mark != index)
                         {
-                            builder.append("print(`").append(content.substring(mark, index)).append("`);\r\n");
+                            builder.append("print(\"").append(content.substring(mark, index)).append("\");\r\n");
                         }
                         mark = index += 2;
                         type = IN_CODE_AREA;
@@ -299,7 +299,7 @@ public class Template
         }
         if (mark != index)
         {
-            builder.append("print(`").append(content.substring(mark, index)).append("`);\r\n");
+            builder.append("print(\"").append(content.substring(mark, index)).append("\");\r\n");
         }
         return new Template(Expression.parseMutli(builder.toString()));
     }
