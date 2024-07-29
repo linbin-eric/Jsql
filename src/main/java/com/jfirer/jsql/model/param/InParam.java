6b0b86e95efa74e1ec831518b89a2e5db5ff1a6f
linbin <4961397@qq.com>
2024-07-29 15:54:54 +0800

    修正 InParam 的 bug

--- src/main/java/com/jfirer/jsql/model/param/InParam.java
+++ src/main/java/com/jfirer/jsql/model/param/InParam.java
@@ -3,8 +3,10 @@ package com.jfirer.jsql.model.param;
 import com.jfirer.jsql.model.Model;
 import com.jfirer.jsql.model.support.SFunction;
 
+import javax.swing.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.stream.Collectors;
 
 public class InParam extends InternalParamImpl
@@ -17,30 +19,93 @@ public class InParam extends InternalParamImpl
         super(fn);
         consumer = (columnName, builder, paramValues) -> {
             builder.append(columnName).append(mode);
-            record WrapperData(String segment, Object paramValue) {}
-            String segment = Arrays.stream(values)//
-                                   .map(value -> {
-                                       if (value instanceof Model m)
-                                       {
-                                           Model.ModelResult result = m.getResult();
-                                           return new WrapperData("(" + result.sql() + ")", result.paramValues());
-                                       }
-                                       else
-                                       {
-                                           return new WrapperData("?", value);
-                                       }
-                                   })//
-                                   .peek(data -> {
-                                       if (data.paramValue instanceof List<?> l)
-                                       {
-                                           paramValues.addAll(l);
-                                       }
-                                       else
-                                       {
-                                           paramValues.add(data.paramValue);
-                                       }
-                                   }).map(data -> data.segment).collect(Collectors.joining(","));
-            builder.append(segment).append(" )");
+            record WrapperData(String segment, Object paramValue)
+            {
+            }
+            if (values.length > 1)
+            {
+                String segment = Arrays.stream(values)//
+                                       .map(value -> {
+                                           if (value instanceof Model m)
+                                           {
+                                               Model.ModelResult result = m.getResult();
+                                               return new WrapperData("(" + result.sql() + ")", result.paramValues());
+                                           }
+                                           else
+                                           {
+                                               return new WrapperData("?", value);
+                                           }
+                                       })//
+                                       .peek(data -> {
+                                           if (data.paramValue instanceof List<?> l)
+                                           {
+                                               paramValues.addAll(l);
+                                           }
+                                           else
+                                           {
+                                               paramValues.add(data.paramValue);
+                                           }
+                                       }).map(data -> data.segment).collect(Collectors.joining(","));
+                builder.append(segment).append(" )");
+            }
+            else
+            {
+                if (values[0] instanceof int[] array)
+                {
+                    builder.append("?");
+                    paramValues.add(array[0]);
+                    for (int i = 1; i < array.length; i++)
+                    {
+                        builder.append(",?");
+                        paramValues.add(array[i]);
+                    }
+                }
+                else if (values[0] instanceof long[] array)
+                {
+                    builder.append("?");
+                    paramValues.add(array[0]);
+                    for (int i = 1; i < array.length; i++)
+                    {
+                        builder.append(",?");
+                        paramValues.add(array[i]);
+                    }
+                }
+                else if (values[0] instanceof Integer[] array)
+                {
+                    builder.append("?");
+                    paramValues.add(array[0]);
+                    for (int i = 1; i < array.length; i++)
+                    {
+                        builder.append(",?");
+                        paramValues.add(array[i]);
+                    }
+                }
+                else if (values[0] instanceof Long[] array)
+                {
+                    builder.append("?");
+                    paramValues.add(array[0]);
+                    for (int i = 1; i < array.length; i++)
+                    {
+                        builder.append(",?");
+                        paramValues.add(array[i]);
+                    }
+                }
+                else if (values[0] instanceof String[] array)
+                {
+                    builder.append("?");
+                    paramValues.add(array[0]);
+                    for (int i = 1; i < array.length; i++)
+                    {
+                        builder.append(",?");
+                        paramValues.add(array[i]);
+                    }
+                }
+                else
+                {
+                    throw new IllegalArgumentException("无法识别的类型:" + values[0].getClass().getName());
+                }
+                builder.append(" )");
+            }
         };
     }
 }
